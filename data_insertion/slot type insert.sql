a7e375923f7372619885a83cef84f302e1bb4990
Hemantharajan V <Hemantharajan.V@mrcooper.com>
2019-06-03 12:58:39 +0530

    inserting duplicate data for tables in database

---
+++ data_insertion/slot type insert.sql
@@ -0,0 +1,4 @@
+insert into [dbo].[slot type] values(1,'small',0,20)
+insert into [dbo].[slot type] values(2,'medium',0,30)
+insert into [dbo].[slot type] values(3,'large',0,40)
+insert into [dbo].[slot type] values(4,'bike',0,5)
