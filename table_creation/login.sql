b16d4a64310f65870a101b234e3478909e90816f
Hemantharajan V <Hemantharajan.V@mrcooper.com>
2019-06-03 12:47:25 +0530

    creating login tale in database

---
+++ table_creation/login.sql
@@ -0,0 +1,26 @@
+USE [multi level parking]
+GO
+
+/****** Object:  Table [dbo].[login]    Script Date: 5/31/2019 6:00:33 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[login](
+	[login ID] [int] NOT NULL primary key,
+	[FirstName] [varchar](30) not NULL,
+	[LastName] [varchar](30) NULL,
+	[mailid] [varchar](50) NOT NULL unique,
+	[phone] [varchar](13) NOT NULL unique,
+	[password] [varchar](16) NOT NULL,
+	[DOB] [date] NULL,
+	[gender] [char](1) NULL,
+	[company name] [varchar](50) NULL,
+	[address] [varchar](200) NULL,
+	[city] [varchar](20) NULL,
+	[pin] [varchar](8) NULL,
+	[wallet balance] [int] NOT NULL,
+
+);
